352d5683c22a36a8c9c67d74e45f41042b0a6d12
Trannick <nicktran29@gmail.com>
2019-02-11 16:14:47 -0800

    fixed import error

--- app/components/TabBarIcon.js
+++ app/components/TabBarIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Icon } from "expo";
 
-import Colors from "../constants/Colors";
+import Colors from "../constants/Colors.js";
 
 export default class TabBarIcon extends React.Component {
   render() {
