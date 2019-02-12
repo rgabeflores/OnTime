d8f2cd112fbae275f645440ec754baa65c0da241
rgabeflores <rgabeflores@gmail.com>
2019-02-11 16:19:02 -0800

    Added more view compression on typing

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -120,7 +120,7 @@ const styles = StyleSheet.create({
   },
   containerCompressed:{
     flex: 1,
-    marginBottom: "35%",
+    marginBottom: "45%",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start'
