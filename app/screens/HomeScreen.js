26f4f9d40e0efb53c820e0d6456059bed3a0b7b2
Carl Costa <32249778+totatmeister@users.noreply.github.com>
2019-02-11 16:57:57 -0800

    Revert again

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,11 +1,93 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+=======
+import React from "react";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { WebBrowser } from "expo";
+
+import { MonoText } from "../components/StyledText";
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+>>>>>>> parent of 0afe9b0... more merge conflicts
 
 export class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+<<<<<<< HEAD
         <Text>This is the home screen</Text>
+=======
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require("../assets/images/robot-dev.png")
+                  : require("../assets/images/robot-prod.png")
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            {this._maybeRenderDevelopmentModeWarning()}
+
+            <Text style={styles.getStartedText}>Get started by opening</Text>
+
+            <View
+              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+            >
+              <MonoText style={styles.codeHighlightText}>
+                screens/HomeScreen.js
+              </MonoText>
+            </View>
+
+            <Text style={styles.getStartedText}>
+              Change this text and your app will automatically reload.
+            </Text>
+          </View>
+
+          <View style={styles.helpContainer}>
+            <TouchableOpacity
+              onPress={this._handleHelpPress}
+              style={styles.helpLink}
+            >
+              <Text style={styles.helpLinkText}>
+                Help, it didn’t automatically reload!
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <View style={styles.tabBarInfoContainer}>
+          <Text style={styles.tabBarInfoText}>
+            This is a tab bar. You can edit it in:
+          </Text>
+
+          <View
+            style={[styles.codeHighlightContainer, styles.navigationFilename]}
+          >
+            <MonoText style={styles.codeHighlightText}>
+              navigation/MainTabNavigator.js
+            </MonoText>
+          </View>
+        </View>
+>>>>>>> parent of 0afe9b0... more merge conflicts
       </View>
     );
   }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,18 +1,7 @@
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableHighlight,
-  Alert,
-  StatusBar,
-  Platform
-} from "react-native";
-import firebase from "firebase";
-import { db } from "../config/db";
-import AppNavigator from "../navigation/AppNavigator";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableHighlight, Alert} from 'react-native';
+import firebase from 'firebase';
+import { db } from '../config/db';
 
 export class LoginScreen extends React.Component {
   constructor(props) {
@@ -20,83 +9,58 @@ export class LoginScreen extends React.Component {
     this.state = {
       email: "",
       password: "",
-      isTyping: false,
-      loginSuccess: true
+      isTyping: false
     };
   }
 
   render() {
-    if (this.state.loginSuccess) {
-      return (
+    return (
+      <View style={(this.state.isTyping) ? styles.containerCompressed : styles.container}>
         <View style={styles.container}>
-          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    } else {
-      return (
-        <View
-          style={
-            this.state.isTyping ? styles.containerCompressed : styles.container
-          }
-        >
-          <View style={styles.container}>
-            <View>
-              <Image
-                source={require("../assets/stopwatch_vector.png")}
-                style={styles.image}
-              />
-            </View>
-            <Text style={styles.Title}> On Time </Text>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                placeholder={"Email"}
-                onChangeText={email => this.setState({ email })}
-                editable={true}
-                maxLength={40}
-                keyboardType={"email-address"}
-                onFocus={this.compressViews.bind(this)}
-                onBlur={this.decompressViews.bind(this)}
-              />
-            </View>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                placeholder={"Password"}
-                onChangeText={password => this.setState({ password })}
-                password={true}
-                editable={true}
-                maxLength={40}
-                secureTextEntry
-                onFocus={this.compressViews.bind(this)}
-                onBlur={this.decompressViews.bind(this)}
-              />
-            </View>
-            <TouchableHighlight
-              style={styles.buttonContainer}
-              onPress={this.logIn.bind(this)}
-              underlayColor="white"
-            >
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Log In</Text>
-              </View>
-            </TouchableHighlight>
-            <TouchableHighlight
-              style={styles.buttonContainer}
-              onPress={this.register.bind(this)}
-              underlayColor="white"
-            >
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Register</Text>
-              </View>
-            </TouchableHighlight>
+          <View>
+            <Image source={require('../assets/stopwatch_vector.png')} style={styles.image}/>
           </View>
+          <Text style={styles.Title}> On Time </Text>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholder={"Email"}
+              onChangeText={(email) => this.setState({email})}
+              editable={true}
+              maxLength={40}
+              keyboardType={"email-address"}
+              onFocus={this.compressViews.bind(this)}
+              onBlur={this.decompressViews.bind(this)}
+            />
+          </View>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholder={"Password"}
+              onChangeText={(password) => this.setState({password})}
+              password={true}
+              editable={true}
+              maxLength={40}
+              secureTextEntry
+              onFocus={this.compressViews.bind(this)}
+              onBlur={this.decompressViews.bind(this)}
+            />
+          </View>
+          <TouchableHighlight style={styles.buttonContainer} onPress={this.logIn.bind(this)} underlayColor="white">
+            <View style={styles.button}>
+              <Text style={styles.buttonText} >Log In</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.buttonContainer} onPress={this.register.bind(this)} underlayColor="white">
+            <View style={styles.button}>
+              <Text style={styles.buttonText} >Register</Text>
+            </View>
+          </TouchableHighlight>
         </View>
-      );
-    }
+      </View>
+    );
   }
 
   // Log In Method
-  logIn = e => {
+  logIn = (e) => {
     let email = this.state.email;
     let password = this.state.password;
     console.log("Email: " + email);
@@ -104,36 +68,30 @@ export class LoginScreen extends React.Component {
     // db.ref('/x').push
     // this pushes '/x' as the 'folder name'
     // then stores the Email as 'name'
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(
-        function(firebaseUser) {
-          console.log("logged in!");
-          navigate("Home");
-        },
-        function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          alert(errorMessage);
-          console.log(error);
-        }
-      );
-  };
-  register = e => {
-    let email = this.state.email;
-    let password = this.state.password;
-    console.log("Email: " + email);
-    console.log("Password: " + password);
-    let firebaseAuth = firebase.auth();
-    let userCredential = firebaseAuth
-      .createUserWithEmailAndPassword(email, password)
-      .then(
+    firebase.auth().signInWithEmailAndPassword(email,password).then(
+      function(firebaseUser){
+        console.log("logged in!")
+        navigate("Home");
+      },
+      function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+        console.log(error);
+    });
+  }
+  register = (e) => {
+      let email = this.state.email;
+      let password = this.state.password;
+      console.log("Email: " + email);
+      console.log("Password: " + password);
+      let firebaseAuth = firebase.auth();
+      let userCredential = firebaseAuth.createUserWithEmailAndPassword(email, password).then(
         // This function is called when createUserWithEmailAndPassword() returns successfully
-        function(firebaseUser) {
+        function(firebaseUser){
           let userID = firebaseAuth.currentUser.uid;
 
-          db.ref("Accounts/" + userID).set({
+          db.ref('Accounts/' + userID).set({
             email: email,
             password: password
           });
@@ -143,30 +101,44 @@ export class LoginScreen extends React.Component {
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-          if (errorCode == "auth/weak-password") {
-            alert("The password is too weak.");
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
           } else {
             alert(errorMessage);
           }
           console.log(error);
-        }
-      );
-  };
-  compressViews = e => {
-    this.setState({ isTyping: true });
-  };
-  decompressViews = e => {
-    this.setState({ isTyping: false });
-  };
+      });
+  }
+  compressViews = (e) =>{
+    this.setState({isTyping : true})
+  }
+  decompressViews = (e) =>{
+    this.setState({isTyping : false})
+  }
+
+  register = () =>{
+    firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(
+      email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        });
+  }
 }
 
 function asyncRequest(url) {
   return fetch(url)
-    .then(response => response.json())
-    .then(responseJson => {
+    .then((response) => response.json())
+    .then((responseJson) => {
       return responseJson.movies;
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
 }
@@ -174,19 +146,19 @@ function asyncRequest(url) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
-    // alignItems: "center",
-    // justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  containerCompressed: {
+  containerCompressed:{
     flex: 1,
     marginBottom: "35%",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
-  textInputContainer: {
-    borderColor: "lightblue",
+  textInputContainer:{
+    borderColor: 'lightblue',
     borderWidth: 1,
     borderRadius: 30,
     padding: 15,
@@ -194,22 +166,23 @@ const styles = StyleSheet.create({
     width: "100%",
     minWidth: "75%"
   },
-  Title: {
-    color: "lightblue",
-    fontSize: 50
+  Title:{
+    color:'lightblue',
+    fontSize:50
   },
-  image: {
+  image:{
     width: 150,
     height: 150
   },
   button: {
-    alignItems: "center",
-    backgroundColor: "lightblue",
-    borderRadius: 30
+   alignItems: 'center',
+   backgroundColor: 'lightblue',
+   borderRadius: 30,
+
   },
   buttonText: {
     padding: 20,
-    color: "white"
+    color: 'white'
   },
   buttonContainer: {
     margin: 5,
