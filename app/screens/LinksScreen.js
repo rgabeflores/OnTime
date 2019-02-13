fa4d60a30a5201fb9a3270c9267e69bada8449a8
trannick <nicktran29@gmail.com>
2019-02-13 00:42:02 -0800

    hopefully fixed everything

--- app/screens/LinksScreen.js
+++ app/screens/LinksScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import {
   StyleSheet,
@@ -14,13 +15,19 @@ import styles from './style';
 
 import firebase from 'firebase';
 import { db } from '../config/db';
+=======
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+// import { ExpoLinksView } from "@expo/samples";
+>>>>>>> hopefully fixed everything
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+  // static navigationOptions = {
+  //   title: "Links"
+  // };
 
   render() {
+<<<<<<< HEAD
     return (
       <ScrollView contentContainerStyle={styles.container}>
           <Text>
@@ -31,3 +38,16 @@ export default class LinksScreen extends React.Component {
   }
 }
 
+=======
+    return <Text>Linkscreen</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: "#fff"
+  }
+});
+>>>>>>> hopefully fixed everything
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -24,46 +24,76 @@ export class LoginScreen extends React.Component {
     this.state = {
       email: "",
       password: "",
-      isTyping: false
+      isTyping: false,
+      loginSuccess: true
     };
   }
 
   render() {
-    return (
-      <View style={(this.state.isTyping) ? styles.containerCompressed : styles.container}>
-        <View style={styles.container}>
-          <View>
-            <Image source={require('../assets/stopwatch_vector.png')} style={styles.image}/>
-          </View>
-          <Text style={styles.Title}> On Time </Text>
-          <View style={styles.textInputContainer}>
-            <TextInput
-              placeholder={"Email"}
-              onChangeText={(email) => this.setState({email})}
-              editable={true}
-              maxLength={40}
-              keyboardType={"email-address"}
-              onFocus={this.compressViews.bind(this)}
-              onBlur={this.decompressViews.bind(this)}
-            />
-          </View>
-          <View style={styles.textInputContainer}>
-            <TextInput
-              placeholder={"Password"}
-              onChangeText={(password) => this.setState({password})}
-              password={true}
-              editable={true}
-              maxLength={40}
-              secureTextEntry
-              onFocus={this.compressViews.bind(this)}
-              onBlur={this.decompressViews.bind(this)}
-            />
-          </View>
-          <TouchableHighlight style={styles.buttonContainer} onPress={this.logIn.bind(this)} underlayColor="white">
-            <View style={styles.button}>
-              <Text style={styles.buttonText} >Log In</Text>
+    if (this.state.loginSuccess) {
+      return (
+        <View style={styles.tabContainer}>
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={
+            this.state.isTyping ? styles.containerCompressed : styles.container
+          }
+        >
+          <View style={styles.container}>
+            <View>
+              <Image
+                source={require("../assets/stopwatch_vector.png")}
+                style={styles.image}
+              />
             </View>
-          </TouchableHighlight>
+          
+            <Text style={styles.Title}> On Time </Text>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                placeholder={"Email"}
+                onChangeText={email => this.setState({ email })}
+                editable={true}
+                maxLength={40}
+                keyboardType={"email-address"}
+                onFocus={this.compressViews.bind(this)}
+                onBlur={this.decompressViews.bind(this)}
+              />
+            </View>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                placeholder={"Password"}
+                onChangeText={password => this.setState({ password })}
+                password={true}
+                editable={true}
+                maxLength={40}
+                secureTextEntry
+                onFocus={this.compressViews.bind(this)}
+                onBlur={this.decompressViews.bind(this)}
+              />
+            </View>
+            <TouchableHighlight
+              style={styles.buttonContainer}
+              onPress={this.logIn.bind(this)}
+              underlayColor="white"
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Log In</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.buttonContainer}
+              onPress={this.register.bind(this)}
+              underlayColor="white"
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Register</Text>
+              </View>
+            </TouchableHighlight>
           <View>
             <TouchableHighlight onPress={this.goToRegister.bind(this)} underlayColor="white">
               <View>
@@ -72,14 +102,14 @@ export class LoginScreen extends React.Component {
             </TouchableHighlight>
           </View>
         </View>
-      </View>
-    );
+      );
+    }
   }
   goToRegister = (e) => {
     this.props.navigation.navigate('Register');
   }
   // Log In Method
-  logIn = (e) => {
+  logIn = e => {
     let email = this.state.email;
     let password = this.state.password;
     console.log("Email: " + email);
@@ -94,16 +124,6 @@ export class LoginScreen extends React.Component {
   }
 }
 
-function asyncRequest(url) {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      return responseJson.movies;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
 
 // const styles = StyleSheet.create({
 //   container: {
