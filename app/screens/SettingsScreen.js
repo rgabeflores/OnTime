6bf2be15acc3f0aab310a8532a93dc95ecec91ea
tecson27 <dawatecson@yahoo.com>
2019-02-27 14:51:41 -0800

    basic settings screen

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -3,6 +3,7 @@ import {
   Text,
   View
 } from 'react-native';
+import SettingsList from 'react-native-settings-list';
 
 import styles from './style';
 
@@ -10,14 +11,36 @@ export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'Settings',
   };
+  constructor(){
+  super();
+  this.onValueChange = this.onValueChange.bind(this);
+  this.state = {switchValue: false};
+  }
 
   render() {
-
     return (
-        <View>
-            <Text>Settings Screen</Text>
-        </View>
+      <View style={{backgroundColor:'gray',flex:1}}>
+      <View style={{flex:1, marginTop:20}}>
+        <SettingsList>
+        	<SettingsList.Header headerText='My Account' headerStyle={{color:'white'}}/>
+          <SettingsList.Item title='Account Info'/>
+
+          <SettingsList.Header headerText='Notifications' headerStyle={{color:'white', marginTop:20}}/>
+          <SettingsList.Item
+            hasNavArrow={false}
+            switchState={this.state.switchValue}
+            switchOnValueChange={this.onValueChange}
+            hasSwitch={true}
+            title='Switch Example'/>
+
+        </SettingsList>
+      </View>
+    </View>
+
 
         );
+  }
+  onValueChange(value){
+  this.setState({switchValue: value});
   }
 }
