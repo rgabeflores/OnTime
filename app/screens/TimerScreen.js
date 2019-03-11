631adc53f9fe7dc39d0096757479fd47afd880bf
tecson27 <dawatecson@yahoo.com>
2019-03-11 16:55:35 -0700

    added comments to Timer

--- app/screens/TimerScreen.js
+++ app/screens/TimerScreen.js
@@ -10,6 +10,7 @@ export class TimerScreen extends React.Component {
     title: "Timer"
   };
   constructor(props) {
+    //initialize variables
     super(props);
     this.state = {
       timerStart: false,
@@ -24,18 +25,22 @@ export class TimerScreen extends React.Component {
     this.resetStopwatch = this.resetStopwatch.bind(this);
   }
 
+  //Toggles timer between start and stop functions
   toggleTimer() {
     this.setState({timerStart: !this.state.timerStart, timerReset: false});
   }
 
+  //Resets timer to set value
   resetTimer() {
     this.setState({timerStart: false, timerReset: true});
   }
 
+  //Toggles stopwatch between start and stop fuctions
   toggleStopwatch() {
     this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
   }
 
+  //Resets stopwatch back to zero
   resetStopwatch() {
     this.setState({stopwatchStart: false, stopwatchReset: true});
   }
@@ -51,30 +56,36 @@ export class TimerScreen extends React.Component {
 
         <Text style={{fontSize: 30}}>Stopwatch</Text>
 
+        {/*Component which shows current value of stopwatch */}
         <Stopwatch laps msecs start={this.state.stopwatchStart}
           reset={this.state.stopwatchReset}
           options={options}
           getTime={this.getFormattedTime} />
 
+        {/*Button that toggles between Start and Stop*/}
         <TouchableHighlight onPress={this.toggleStopwatch}>
           <Text style={{fontSize: 30,left:120,}}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
         </TouchableHighlight>
 
+        {/*Button that resets stopwatch back down to zero*/}
         <TouchableHighlight onPress={this.resetStopwatch}>
           <Text style={{fontSize: 30,left:120,}}>Reset</Text>
         </TouchableHighlight>
 
         <Text style={{fontSize: 30}}>Timer</Text>
 
+        {/*Component which shows current value of timer*/}
         <Timer totalDuration={this.state.totalDuration} msecs start={this.state.timerStart}
           reset={this.state.timerReset}
           options={options}
           getTime={this.getFormattedTime} />
 
+        {/*Button that toggles between Start and Stop*/}
         <TouchableHighlight onPress={this.toggleTimer}>
           <Text style={{fontSize: 30,left:120,}}>{!this.state.timerStart ? "Start" : "Stop"}</Text>
         </TouchableHighlight>
 
+        {/*Button that resets timer back up to set value*/}
         <TouchableHighlight onPress={this.resetTimer}>
           <Text style={{fontSize: 30,left:120,}}>Reset</Text>
         </TouchableHighlight>
@@ -84,7 +95,7 @@ export class TimerScreen extends React.Component {
     );
   }
 }
-
+  //style for timer screen
   const options = {
   container: {
     backgroundColor: 'lightblue',
