f337603489459c19dd61af33ea9da63850e89130
rgabeflores <rgabeflores@gmail.com>
2019-04-08 01:22:59 -0700

    Moved renderRow to new TaskRow class

---
+++ app/screens/components/TaskRow.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  Platform
+} from "react-native";
+
+import Icon from "../../components/TabBarIcon";
+import styles from "../../components/style";
+import otherStyles from "../style";
+
+export default TaskRow = (props) => {
+    return (
+        <View>
+          <View style={otherStyles.taskContainer}>
+            <Text style={styles.liText}>
+              Task Name: {props.task.title} {"\n"}
+              Required Time: {props.task.hours} {"\n"}
+              Location: {props.task.address}
+            </Text>
+            <TouchableHighlight
+              style={props.deleteMode? {
+                      display: "flex",
+                      alignItems: "center"
+                    }: {display: "none"}
+              }
+              onPress={(task) => {
+                props.press(task);
+              }}
+            >
+              <Icon
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-close-circle-outline"
+                    : "md-close-circle-outline"
+                }
+              />
+            </TouchableHighlight>
+          </View>
+        </View>
+      );
+}
