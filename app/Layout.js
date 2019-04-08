8b3c5b369a8e2ccc6a84d72b7e73e71bf08bbaa0
rgabeflores <rgabeflores@gmail.com>
2019-04-08 01:12:43 -0700

    fixed merge conflict

--- app/Layout.js
+++ app/Layout.js
@@ -14,18 +14,11 @@ export class Layout extends React.Component {
 }
 
 // create map of "store" object passed from Provider to this component's props
-<<<<<<< HEAD
-const mapStateToProps = store => {
-  return {
-    isLoggedIn: store.user.isLoggedIn // store.user == reducer, store.user.user == reducer.state.user
-=======
 const mapStateToProps = (store) => {
     return {
       isLoggedIn: store.user.isLoggedIn, // Enables login status and redirection from login/register screens to home page
       error: store.user.error
     }
->>>>>>> ef6faad6e0d2c65695b351f21195f609dc641f5b
-  };
 };
 
 // connect() applies maps to component's props
